d79d436d3987d6815094ae3c533099f34047e6d7
Maureen E. Zitouni <maureen.zitouni@gmail.com>
2025-10-14 00:20:17 -0400

    Update logic to handle multiple clicks + styling

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 // create a list of items with duplicates - should be even
-const list = ["a", "a", "b", "b", "c", "c", "d", "d", "e", "e", "f", "f"];
+const list = [
+  "image0.png",
+  "image0.png",
+  "image1.png",
+  "image1.png",
+  "image2.png",
+  "image2.png",
+  "image3.png",
+  "image3.png",
+  "image4.png",
+  "image4.png",
+  "image5.png",
+  "image5.png",
+];
 const TOTAL_CARD_COUNT = list.length;
 
 // shuffle list - used Fisher-Yates algorithm: https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
@@ -11,52 +24,61 @@ const shuffle = (arr) => {
   return arr;
 };
 const gameBoard = shuffle(list);
-console.log(gameBoard);
 
-// create board of cards
-let index = 0;
-
+// create board of cards in the DOM
 for (let i = 0; i < TOTAL_CARD_COUNT; i++) {
   const card = document.createElement("div");
   card.classList.add("card");
   card.onclick = () => handleCardClick(card, i);
-  document.querySelector("main").appendChild(card);
-  index++;
+  document.querySelector("section").appendChild(card);
 }
 
 let lastFlipped = {};
 let totalMatches = 0;
+let lockBoard = false;
+let points = 0;
 
 function handleCardClick(card, index) {
+  // prevent double-clicking the same card
+  if (card.classList.contains("active")) return;
+
+  // prevent clicking more than 2 cards while matches are being checked
+  if (lockBoard) return;
+
   // flip card
   card.classList.add("active");
+  card.style.backgroundImage = `url('./images/${gameBoard[index]}')`;
 
-  if (!Object.keys(lastFlipped).length) {
+  if (!lastFlipped.card) {
     lastFlipped = { index, card };
   } else {
     const firstCard = lastFlipped.card;
 
     // check if match
-    console.log("checking if match");
-
+    lockBoard = true;
     setTimeout(() => {
       if (gameBoard[lastFlipped.index] === gameBoard[index]) {
         // disable click + leave flipped
-        console.log("MATCH");
+        points++;
+        document.getElementById("points").innerText = points;
         totalMatches++;
         firstCard.onclick = null;
-        lastFlipped = [];
+        lastFlipped = {};
 
         // check if game is complete
         if (totalMatches === TOTAL_CARD_COUNT / 2) {
-          console.log("YOU WIN");
+          const h2 = document.createElement("h2");
+          h2.innerText = "YOU WIN!";
+          document.querySelector("section").appendChild(h2);
         }
       } else {
-        console.log("NOT A MATCH");
         firstCard.classList.remove("active");
+        firstCard.style.backgroundImage = "unset";
         card.classList.remove("active");
-        lastFlipped = [];
+        card.style.backgroundImage = "unset";
+        lastFlipped = {};
       }
-    }, 1000);
+      lockBoard = false;
+    }, 500);
   }
 }
